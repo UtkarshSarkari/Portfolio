3fce44d6942a52af1e33c0e70611ed0d866cf190
UtkarshSarkari <utkrsh2001@gmail.com>
2023-08-26 02:32:45 +0530

    fourth commit

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,9 +1,11 @@
+import Image from "next/image";
 
 export default function Footer() {
   return (
-    <div className="fixed bottom-0 z-50 font-mainFont flex flex-col items-center justify-center border-t border-gray-800 text-center p-2">
-        <span className="text-[15px] mb-1">Desclaimer</span>
-        <p className="text-[12px] italic">This app is created by Utkarsh Sarkari and is currently hosted on Vercel. Technology used to create this app are Next.js 13, TypeScript and Tailwind CSS. All rights are reserved under CC</p>
+    <div className="fixed bottom-0 bg-black z-50 flex items-center justify-center w-screen p-5 space-x-2">
+        <p className="">Made with <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 inline-block">
+  <path d="M9.653 16.915l-.005-.003-.019-.01a20.759 20.759 0 01-1.162-.682 22.045 22.045 0 01-2.582-1.9C4.045 12.733 2 10.352 2 7.5a4.5 4.5 0 018-2.828A4.5 4.5 0 0118 7.5c0 2.852-2.044 5.233-3.885 6.82a22.049 22.049 0 01-3.744 2.582l-.019.01-.005.003h-.002a.739.739 0 01-.69.001l-.002-.001z" />
+</svg> by Utkarsh Sarkari</p>
     </div>
   )
 }
